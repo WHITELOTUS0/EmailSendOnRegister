93faf25be9a46f9db187d4c9bd5392f97c11c930
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-07-05 11:58:55 +0300

    updates

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 // Create MySQL connection pool
@@ -6,7 +6,14 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-module.exports = pool;
+// Retrieve a connection from the pool
+const connection = pool.promise();
+
+module.exports = connection;
+
